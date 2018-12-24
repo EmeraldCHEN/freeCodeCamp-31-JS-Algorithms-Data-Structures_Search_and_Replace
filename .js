43b44e59a63546651b3f108836b4f2119022efbe
EmeraldNZ <ahyuancy@gmail.com>
2018-12-24 21:01:46 +1300

    Update .js

--- .js
+++ .js
@@ -1,7 +1,8 @@
 function myReplace(str, before, after) {
-  if(before[0].match(/[A-Z]/) && !after[0].match(/[A-Z]/)){
+  
+   if(before[0].match(/[A-Z]/) && !after[0].match(/[A-Z]/)){
    
-   after = after[0].toUpperCase() + after.slice(1); // Same as: after = after.charAt(0).toUpperCase() + after.slice(1);
-  }
-  return str.replace(before, after);
+       after = after[0].toUpperCase() + after.slice(1); // Same as: after = after.charAt(0).toUpperCase() + after.slice(1);
+   }
+   return str.replace(before, after);
 }
